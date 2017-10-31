3149938dfb38f5436e2c47b31f8d1a5129eb9bfe
figoxu <xujh945@qq.com>
2017-10-31 10:54:05 +0800

    模块化vuex信息

--- src/router/vuexui.js
+++ src/router/vuexui.js
@@ -5,11 +5,18 @@ export default {
     },
     redirect:'/vuex/counter',
     component(resolve) {
-        require.ensure(['views/vuex/counter.vue'], () => {
-            resolve(require('views/vuex/counter.vue'));
+        require.ensure(['views/vuex/index.vue'], () => {
+            resolve(require('views/vuex/index.vue'));
         })
     },
     children:[{
+        path:'/vuex/counter',
+        component(resolve){
+            require.ensure(['views/vuex/counter.vue'], () => {
+                resolve(require('views/vuex/counter.vue'));
+            })
+        }
+    },{
         path:'/vuex/counter',
         component(resolve){
             require.ensure(['views/vuex/counter.vue'], () => {
---
+++ src/views/vuex/index.vue
@@ -0,0 +1,24 @@
+<template>
+  
+    <div class="body">
+
+        <div class="left_col ">
+
+            <!--一级导航区域-->
+            <div class="left_link">
+
+                <div class="leftBoard">
+                    <span class="icon"></span>
+                    <span class='sideTitle'>Sidebars</span>
+                    <span class='info'>new</span>
+                </div>
+                <ul>
+                    <li><router-link to='/vuex/counter'>Counter</router-link></li>
+                   
+                </ul>
+            </div>
+
+        </div>
+        <router-view></router-view>
+    </div>
+</template>

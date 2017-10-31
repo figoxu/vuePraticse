0786da57ce972453ec599d1c1ad178c05887c28e
figoxu <xujh945@qq.com>
2017-10-31 11:05:17 +0800

    为vuex增加日志插件

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import count from './modules/count'
+import createLogger from '../common/plugin/logger'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     modules: {
       count
-    }
+    },
+    plugins:[createLogger()]
   })
   
